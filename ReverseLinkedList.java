de3c6a788092f1cd0eac4582288a84cb67923251
Ishan Sahu <55119806+ishansahu@users.noreply.github.com>
2019-09-24 22:43:35 -0400

    Add files via upload

---
+++ ReverseLinkedList.java
@@ -0,0 +1,28 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1671\cocoasubrtf600
+{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 /**\
+ * Definition for singly-linked list.\
+ * public class ListNode \{\
+ *     int val;\
+ *     ListNode next;\
+ *     ListNode(int x) \{ val = x; \}\
+ * \}\
+ */\
+class Solution \{\
+    public ListNode reverseList(ListNode head) \{\
+        ListNode prev = null;\
+        ListNode curr = head;\
+        while(curr != null)\{\
+            ListNode temp = curr.next;\
+            curr.next = prev;\
+            prev = curr;\
+            curr = temp;\
+        \}\
+        return prev;\
+    \}\
+\}}
