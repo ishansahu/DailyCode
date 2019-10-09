4de0f7cec13e278915ed7a493a19e2d9a808de30
Ishan Sahu <55119806+ishansahu@users.noreply.github.com>
2019-10-08 23:25:03 -0400

    Add files via upload

---
+++ SwapNodesinPairs.java
@@ -0,0 +1,35 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1671\cocoasubrtf600
+{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 /**\
+ * Definition for singly-linked list.\
+ * public class ListNode \{\
+ *     int val;\
+ *     ListNode next;\
+ *     ListNode(int x) \{ val = x; \}\
+ * \}\
+ */\
+class Solution \{\
+    public ListNode swapPairs(ListNode head) \{\
+        if(head == null) return null;\
+        ListNode header = new ListNode(0);\
+        header.next = head;\
+        ListNode first = head;\
+        int count =1;\
+        \
+        while(first.next!= null)\{\
+            if(count %2 == 1)\{\
+                int temp = first.val;\
+                first.val = first.next.val;\
+                first.next.val=temp;\
+            \}\
+            first = first.next;\
+            count++;\
+        \}\
+        return header.next;\
+    \}\
+\}}
