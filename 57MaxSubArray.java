6650722cd0bd33d6b524bed223b78b4ef87b5db6
Ishan Sahu <55119806+ishansahu@users.noreply.github.com>
2020-01-07 11:48:29 -0500

    Add files via upload

---
+++ 57MaxSubArray.java
@@ -0,0 +1,17 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1671\cocoasubrtf600
+{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 class Solution \{\
+    public int maxSubArray(int[] nums) \{\
+        int currentSum =0, maxSum = nums[0];\
+        for (int i = 0; i < nums.length ; i++)\{\
+            currentSum = Math.max(nums[i], currentSum + nums[i]);\
+            maxSum = Math.max(maxSum, currentSum);\
+        \}\
+        return maxSum;\
+    \}\
+\}}
