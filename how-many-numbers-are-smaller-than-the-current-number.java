eccf76dc4875c030c6e36a5deed52b74683531e1
Ishan Sahu <55119806+ishansahu@users.noreply.github.com>
2020-03-08 23:27:36 -0400

    Add files via upload

---
+++ how-many-numbers-are-smaller-than-the-current-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] numsCopy = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(numsCopy);
+        int[] res = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            res[i] = binarySearch(numsCopy, nums[i]);
+        }
+        return res;
+    }
+    private int binarySearch(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] < target) l = mid + 1;
+            else r = mid;
+        }
+        return l;
+    }
+}
