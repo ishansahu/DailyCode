b0c7b66ff4370a0223d3041da1558f9b7ae9e6f5
Ishan Sahu <55119806+ishansahu@users.noreply.github.com>
2020-03-19 23:07:10 -0400

    Add files via upload

--- maximum-product-subarray.java
+++ maximum-product-subarray.java
@@ -1,16 +1,15 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int res = nums[0];
-        int min = res, max = res;
-        
+        int max = nums[0], min = nums[0];
+        int res =nums[0];
         for(int i=1; i<nums.length; ++i){
             if(nums[i]<0){
                 int temp = min;
                 min = max;
                 max = temp;
-            }
-            min = Math.min(nums[i], min*nums[i]);
-            max = Math.max(nums[i], max*nums[i]);
+            } 
+            min = Math.min(nums[i], min *nums[i]);
+            max = Math.max(nums[i], max *nums[i]);
             res = Math.max(res, max);
         }
         return res;
