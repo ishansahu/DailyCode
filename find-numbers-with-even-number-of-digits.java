42782b1c1e328f114b09a67cb5d18c881f911723
Ishan Sahu <55119806+ishansahu@users.noreply.github.com>
2020-03-27 23:08:35 -0400

    Add files via upload

--- find-numbers-with-even-number-of-digits.java
+++ find-numbers-with-even-number-of-digits.java
@@ -1,21 +1,8 @@
 class Solution {
-    public int findNumbers_mysol(int[] nums) {
-        int res=0;
-        for(int n : nums){
-            int count=0;
-            while(n>0){
-                ++count;
-                n/=10;
-            }
-            if(count %2 ==0) res++;
-        }
-        return res;
-    }
-    
     public int findNumbers(int[] nums) {
         int res=0;
-        for(int n : nums){
-            if((n>9 && n<100) ||  (n>999 && n<10000))res++;
+        for(int num : nums){
+            if(String.valueOf(num).length()%2==0) ++res;
         }
         return res;
     }
