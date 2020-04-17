2c904cd491647f1a46b970bec5b711421bfee2bd
Ishan Sahu <55119806+ishansahu@users.noreply.github.com>
2020-04-16 23:09:13 -0400

    Add files via upload

---
+++ backspace-string-compar.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean backspaceCompare(String S, String T) {
+        int i= S.length()-1, j=T.length()-1;
+        while(true){
+            int back=0;
+            while(i>=0 && (back >0 ||S.charAt(i) =='#')){
+                back += S.charAt(i)=='#'?1:-1;
+                --i;
+            }
+            back=0;
+            while(j>=0 &&(back>0 ||T.charAt(j) =='#')){
+                back += T.charAt(j) =='#'?1:-1;
+                --j;
+            }
+            if(i<0 || j<0) break;
+            if(S.charAt(i--)!=T.charAt(j--)) return false;
+            
+        }
+        return i==-1 && j==-1;
+    }
+}
