13fb9801792b9cdc3e3c70c359fcb22558b8fc9f
Ishan Sahu <55119806+ishansahu@users.noreply.github.com>
2020-04-25 23:15:24 -0400

    Add files via upload

---
+++ bitwise-and-of-numbers-range.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int rangeBitwiseAnd(int m, int n) {
+        if(m==0) return 0;
+        int index=1;
+        while(m!=n){
+            m>>=1;
+            n>>=1;
+            index<<=1;
+        }
+        return m*index;
+    }
+}
