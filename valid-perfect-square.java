990a6a0cb1c13fdea66cfa95669c17eb338b683b
Ishan Sahu <55119806+ishansahu@users.noreply.github.com>
2020-05-17 23:42:28 -0400

    Add files via upload

--- valid-perfect-square.java
+++ valid-perfect-square.java
@@ -1,6 +1,12 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-        double x = Math.sqrt(num);
-        return (x == (int)x)? true: false;
+        int l=1, r=num/2+1;
+        while(l<r){
+            long mid = l + (r-l)/2;
+            if(mid * mid == num) return true;
+            else if(mid * mid <num) l = (int)mid+1;
+            else r=(int)mid-1;
+        }
+        return l*l==num? true:false;
     }
-}
+}/Users/ishansahu/Downloads/ravenous-part-two-solution/src/components/BusinessList/BusinessList.js
