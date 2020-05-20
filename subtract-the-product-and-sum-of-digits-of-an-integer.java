d31128059f406f8dce3f705da9c2e029a1c0098a
Ishan Sahu <55119806+ishansahu@users.noreply.github.com>
2020-05-19 23:06:45 -0400

    Add files via upload

--- subtract-the-product-and-sum-of-digits-of-an-integer.java
+++ subtract-the-product-and-sum-of-digits-of-an-integer.java
@@ -2,10 +2,11 @@ class Solution {
     public int subtractProductAndSum(int n) {
         int prod=1, sum=0;
         while(n>0){
-            prod *= n%10;
-            sum += n%10;
-            n/=10;
+            int temp = n %10;
+            n /= 10;
+            prod *= temp;
+            sum += temp;
         }
-        return prod-sum;
+        return prod - sum;
     }
 }
