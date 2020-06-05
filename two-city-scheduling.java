0525aa9d9ee7824e4ad3350f47775bff3b92b3c3
Ishan Sahu <55119806+ishansahu@users.noreply.github.com>
2020-06-04 23:25:53 -0400

    Add files via upload

--- two-city-scheduling.java
+++ two-city-scheduling.java
@@ -1,16 +1,37 @@
 class Solution {
-    public int twoCitySchedCost(int[][] costs) {
+    public int twoCitySchedCost_dp(int[][] costs) {
         int n = costs.length/2;
-        int[][] dp = new int[n+1][n+1]; 
-        for(int i=1; i<=n ;++i) dp[i][0] = costs[i-1][0] + dp[i-1][0];
-        for(int i=1; i<=n ;++i) dp[0][i] = costs[i-1][1] + dp[0][i-1];
+        int dp[][] = new int[n+1][n+1];
+        
+        for(int i=1; i<=n; ++i) dp[i][0] = dp[i-1][0] + costs[i-1][0];
+        for(int i=1; i<=n; ++i) dp[0][i] = dp[0][i-1] + costs[i-1][1];
+        
         for(int i=1; i<=n; ++i){
             for(int j=1; j<=n; ++j){
-                dp[i][j] = Math.min((dp[i-1][j] + costs[i+j-1][0]), (dp[i][j-1]+costs[i+j-1][1]));
-                System.out.print(dp[i][j]+" ");
+                dp[i][j] = Math.min((dp[i][j-1]+costs[i+j-1][1]), dp[i-1][j] + costs[i+j-1][0]);
             }
-            System.out.println();
         }
-       return dp[n][n];
+        return dp[n][n];
     }
+    
+    public int twoCitySchedCost(int[][] costs) {
+        PriorityQueue<int[]> pq = new PriorityQueue( new Comparator<int[]>(){
+           @Override
+             public int compare(int[] A, int[] B){
+                return (B[1] - B[0]) - (A[1] -A[0]);
+            }
+        });
+        for(int[] cost : costs) pq.add(cost);
+        int res =0, n = costs.length/2, ind =0;
+        while(!pq.isEmpty()){
+            int[] curr = pq.poll();
+            if(ind <n){
+                res += curr[0];
+                ++ind;
+            }else res+= curr[1];
+        }
+        return res;
+    }
+    
+    
 }
