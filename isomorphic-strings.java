216756e4ec664233887da22b0246d0c286bbe980
Ishan Sahu <55119806+ishansahu@users.noreply.github.com>
2020-09-25 00:19:37 -0400

    Add files via upload

--- isomorphic-strings.java
+++ isomorphic-strings.java
@@ -1,14 +1,23 @@
 class Solution {
-    public boolean isIsomorphic(String s, String t) {
+    public boolean isIsomorphic_1(String s, String t) {
         HashMap<Character, Character> map = new HashMap();
         for(int i=0; i<s.length(); ++i){
-            if(map.containsKey(t.charAt(i))){
-                if (map.get(t.charAt(i)) !=s.charAt(i))  return false;
-            } 
-            else{
-                 if (map.containsValue(s.charAt(i))) return false;
-                map.put(t.charAt(i),s.charAt(i));
-            } 
+            char src = s.charAt(i), trg = t.charAt(i);
+            if(map.containsKey(src)){
+                if(map.get(src) != trg) return false;
+            }else{
+                if(map.containsValue(trg)) return false;
+            }
+            map.put(s.charAt(i),t.charAt(i));
+        }
+        return true;
+    }
+    
+    public boolean isIsomorphic(String s, String t) {
+        int[] arr = new int[512];
+        for(int i=0; i<s.length(); ++i){
+            if(arr[s.charAt(i)] != arr[t.charAt(i)+256]) return false;
+            arr[s.charAt(i)] = arr[t.charAt(i)+256] = i+1;
         }
         return true;
     }
