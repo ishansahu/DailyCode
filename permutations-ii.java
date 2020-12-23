042cd892aa83ff9697601aa7c2f6f49458036311
Ishan Sahu <55119806+ishansahu@users.noreply.github.com>
2020-12-22 23:23:37 -0500

    Add files via upload

--- permutations-ii.java
+++ permutations-ii.java
@@ -1,24 +1,36 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        Arrays.sort(nums);
+        if(nums == null || nums.length==0) return new ArrayList();
+        int n = nums.length;
+        boolean[] arr = new boolean[n];
+        HashSet<String> set = new HashSet();
         List<List<Integer>> res = new ArrayList();
-        helper(res, new ArrayList(), nums, new boolean[nums.length]);
-        return res;
+        Arrays.sort(nums);
+        helper(nums, res, new ArrayList(), set, arr,0);
+        return res;    
     }
     
-    public void helper(List<List<Integer>> res, List<Integer> temp, int[] nums, boolean[] visit){
-        if(temp.size() == nums.length){
-            res.add(new ArrayList(temp));
-        }else{
-            for(int i=0; i<nums.length; ++i){
-                if(visit[i] || i>0 && nums[i] == nums[i-1] && !visit[i-1]) continue;
-                visit[i] =true;
-                temp.add(nums[i]);
-                helper(res, temp, nums,visit);
-                temp.remove(temp.size()-1);
-                visit[i] =false;
-            }    
+    private void helper(int[] nums, List<List<Integer>> res, List<Integer> lst, HashSet<String> set, boolean[]arr, int idx){
+        if(lst.size() == nums.length){
+            StringBuilder sb = new StringBuilder();
+            for(int num : lst){
+                sb.append('*');
+                sb.append(num);
+            }
+            if(!set.contains(sb.toString())){
+                res.add(new ArrayList(lst));
+                set.add(sb.toString());
+            }
+            return;
         }
-        
+        for(int i=0; i<nums.length; ++i){
+            if(arr[i]) continue;
+            if(i>0 && nums[i-1] == nums[i] && !arr[i-1]) continue;
+            lst.add(nums[i]);
+            arr[i] = true;
+            helper(nums,res, lst,set,arr,i+1);
+            lst.remove(lst.size()-1);
+            arr[i] = false;
+        }
     }
 }
